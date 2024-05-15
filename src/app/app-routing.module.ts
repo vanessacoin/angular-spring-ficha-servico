72c6af4274fc1b78d62860bee3a57e4c102bc24f
Vanessa <vanessarcoin@gmail.com>
2024-05-15 14:31:58 -0300

    theme style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'customers'},
+  {
+    path: 'customers',
+    loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
